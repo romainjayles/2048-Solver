418f0603622a6c872e3e9cfc022b44370839c928
Romain Jayles <romain.jayles@orange.com>
2016-11-21 14:14:47 +0100

    Allow to choose between interactive mode and IA mode

--- main.c
+++ main.c
@@ -4,78 +4,68 @@
 #include <string.h>
 
 #include "2048.h"
+#include "minimax_solver.h"
 
-#define INTERACTIVE true
-
-
-
+#define INTERACTIVE
 
 int main(int argc, char **argv){
-  int interactive = INTERACTIVE;
-  int i;
   char ch1, ch2, ch3;
   struct grid main_grid;
-  struct grid move_grid;
-  init_grid(&main_grid);
-  /*main_grid.grid[0] = 0;
-  main_grid.grid[1] = 0;
-  main_grid.grid[2] = 2;
-  main_grid.grid[3] = 0;
+  bool move_sucess;
+  move_type move;
   
-  main_grid.grid[4] = 0;
-  main_grid.grid[5] = 2;
-  main_grid.grid[6] = 0;
-  main_grid.grid[7] = 2;
-
-  main_grid.grid[8] = 2;
-  main_grid.grid[9] = 2;
-  main_grid.grid[10] = 4;
-  main_grid.grid[11] = 8;
-
-  main_grid.grid[12] = 2;
-  main_grid.grid[13] = 4;
-  main_grid.grid[14] = 8;
-  main_grid.grid[15] = 16;*/
+  init_grid(&main_grid);
   put_random_tile(&main_grid, &main_grid);
+  
   print_grid(&main_grid, false);
   while(1){
+    #ifdef INTERACTIVE
     do{
       ch3 = ch2;
       ch2 = ch1;
       ch1 = fgetc(stdin);
-      //printf("\n%i - %i - %i \n", ch3, ch2, ch1);
     }while (!(ch3 == 27 && ch2== 91 && (ch1 == 68 || ch1 == 67 || ch1 == 66 | ch1 == 65)));
     clearerr(stdin);
     switch(ch1){
     case 68:
-      printf("Left\n");
-      move_left(&main_grid, &main_grid);
+      move = MOVE_LEFT;
       break;
     case 67:
-      printf("Right\n");
-      move_right(&main_grid, &main_grid);
+      move = MOVE_RIGHT;
       break;
     case 66:
-      printf("down\n");
-      move_down(&main_grid, &main_grid);
+      move = MOVE_DOWN;
       break;
     case 65:
-      printf("Up\n");
-      move_up(&main_grid, &main_grid);
+      move = MOVE_UP;
       break;
     }
+    #else
+    move = compute_best_move(&main_grid);
+    #endif
+    switch(move){
+    case MOVE_LEFT:
+      move_sucess = move_left(&main_grid, &main_grid);
+      break;
+    case MOVE_RIGHT:
+      move_sucess = move_right(&main_grid, &main_grid);
+      break;
+    case MOVE_UP:
+      move_sucess = move_up(&main_grid, &main_grid);
+      break;
+    case MOVE_DOWN:
+      move_sucess = move_down(&main_grid, &main_grid);
+      break;
+    }
+    if(!move_sucess){
+      printf("This move is impossible\n");
+    }else{
+      put_random_tile(&main_grid, &main_grid);
+    }
     
-    put_random_tile(&main_grid, &main_grid);
     print_grid(&main_grid, false);
   }
   
-  print_grid(&main_grid, true);
-  move_down(&main_grid, &move_grid);
-  print_grid(&move_grid, true);
-  move_down(&move_grid, &move_grid);
-  print_grid(&move_grid, true);
-  move_right(&move_grid, &move_grid);
-  print_grid(&move_grid, true);
   return 0;
 }
 
